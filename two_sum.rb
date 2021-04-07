4ae3b72656577336ceae145463492441b519f9ac
Max Ribbans <ribbans.max@gmail.com>
2021-04-07 11:11:22 -0400

    Add instructions

---
+++ two_sum.rb
@@ -0,0 +1,10 @@
+# Two Sum
+# Given two arguments, the first one being an array and the second argument being the target number.  Return true if any of the two numbers in the array can add up to the target number.
+# Note all numbers in the array will be unique, integers, and non negative.
+# e.g.
+# 1.)  given `[1,3,5,7], 12` your function/method will return true since the number 5 + 7 = 12
+# 2.) Example given `[2,3,4,5], 11` you function/method will return false since no two numbers would equal 11
+
+def two_sum(integers, target) 
+  # your code goes here
+end
